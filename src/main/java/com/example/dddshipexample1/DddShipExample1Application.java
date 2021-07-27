9d2b5bb19ca002e3faa59c7c016b8e998c6b83c7
mbimbij <joseph.mbimbi@gmail.com>
2021-07-27 20:40:35 +0200

    Initial Commit

---
+++ src/main/java/com/example/dddshipexample1/DddShipExample1Application.java
@@ -0,0 +1,13 @@
+package com.example.dddshipexample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddShipExample1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddShipExample1Application.class, args);
+    }
+
+}
