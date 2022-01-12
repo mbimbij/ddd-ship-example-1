308f841296f6a09fcce92f9c4c0dc802cc160007
mbimbij <joseph.mbimbi@gmail.com>
2022-01-12 18:27:42 +0100

    "kata" starting point

---
+++ src/test/java/com/example/StarterIT.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StarterIT {
+
+    @Test
+    void name() {
+        assertThat(true).isFalse();
+    }
+
+}
---
+++ src/test/java/com/example/StarterTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StarterTest {
+
+    @Test
+    void name() {
+        assertThat(true).isFalse();
+    }
+
+}
