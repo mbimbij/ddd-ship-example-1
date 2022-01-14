bcba365e36c2e516de5a9ab4b282890b0e4f89f5
mbimbij <joseph.mbimbi@gmail.com>
2022-01-14 12:31:36 +0100

    Define \ISignOffCargoReception` as a`@FunctionalInterface`

--- src/main/java/com/example/application/port/in/ISignOffCargoReception.java
+++ src/main/java/com/example/application/port/in/ISignOffCargoReception.java
@@ -2,6 +2,7 @@ package com.example.application.port.in;
 
 import com.example.application.CargoSignoffCommand;
 
+@FunctionalInterface
 public interface ISignOffCargoReception {
     void signOff(CargoSignoffCommand cargoSignoffCommand);
 }
